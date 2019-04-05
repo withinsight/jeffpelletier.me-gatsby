4823988b93aa85858a51db1a504493d44280842d
Jeff Pelletier <jpell185@yahoo.com>
2019-04-05 10:31:37 -0400

    basic layout component

--- src/components/header.js
+++ src/components/header.js
@@ -2,8 +2,6 @@ import React from "react"
 import { Link } from "gatsby"
 import styles from "./header.module.css"
 
-console.log(styles);
-
 export default () => (
     <header className={styles.Header}>
         <h1 className={styles.HeaderH1}>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,9 @@
 import React from "react"
+import Layout from "../components/layout"
 import Header from "../components/header"
 
 export default () => (
-    <div className="container">
+    <Layout>
         <Header />
-    </div>
+    </Layout>
 )
