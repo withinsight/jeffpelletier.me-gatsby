90f15bbbdd4498aa31ee953961ce2bfe3e826a6d
Jeff Pelletier <jpell185@yahoo.com>
2019-04-09 14:47:33 -0400

    add Google fonts in html head

--- src/html.js
+++ src/html.js
@@ -11,6 +11,7 @@ export default function HTML(props) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
+        <link href="https://fonts.googleapis.com/css?family=Martel+Sans:400,700,900|Montserrat:300" rel="stylesheet"></link>
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
