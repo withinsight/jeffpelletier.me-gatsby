ad531df403aa36759b1a20a2850c194e934af7b3
Jeff Pelletier <jpell185@yahoo.com>
2019-04-09 14:56:27 -0400

    add link to static portfolio url

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,8 @@ export default () => (
 
         <p>I'm a designer who's been coding professionally for over 15 years. <br /> I love technology, and write about it (when lightning strikes).</p>
 
+        <p>You're viewing the Gatsby version of my portfolio. There's also <a href="http://jeffpelletier.me">a static HTML version</a>, if that's your flavor.</p>
+
         <p>Check out some of my recent <a href="work.html">work</a>, some of the <Link to="writing">words I've written</Link>, and <a href="mailto:jeffrey.k.pelletier+folio@gmail.com">get in touch</a> if you want to chat.</p>
     </Layout>
 )
