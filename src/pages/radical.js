59e2b437b50bd48054b9726d52fb964025081d9d
Jeff Pelletier <jpell185@yahoo.com>
2019-04-09 10:45:11 -0400

    check in "radical" page

---
+++ src/pages/radical.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Layout from "../components/layout"
+import ContentH2 from "../components/contenth2"
+
+export default () => (
+    <Layout>
+        <ContentH2 title="Radical" />
+
+        <p>While I was at Intridea/Mobomo, I led the frontend track of our weekly team meetups. During this time, I presented on a variety of subjects, from OOCSS, to Design Systems to Ionic.</p>
+
+        <p>Check out the deck from my <a data-hook="link-external" href="https://withinsight.github.io/design-systems" rel="external">Design Systems</a> presentation.</p>
+    </Layout>
+)
