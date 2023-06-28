f959b4dde889eb1bc952b4c52de3229baea6f4aa
Jeff Pelletier <jpell185@yahoo.com>
2023-06-28 13:32:06 -0400

    decommussion stencil version

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,13 +13,12 @@ export default () => (
 
         <p>On the latter point, I implemented a feature workflow for design systems team work, collaborated on component specs for engineering handoff, owned metrics for the design systems team, and advocated for design systems internally by hosting watch parties.</p>
 
-        <p>I've been experimenting and built 3 different versions of my portfolio. You're currently viewing the <a data-hook="link-external" href="https://www.gatsbyjs.org/" rel="external">Gatsby</a> version.</p>
+        <p>I've been experimenting and built 2 different versions of my portfolio. You're currently viewing the <a data-hook="link-external" href="https://www.gatsbyjs.org/" rel="external">Gatsby</a> version.</p>
 
-        <p>These additional versions are available (note which version you're viewing via the URL bar):</p>
+        <p>The vanilla HTML version is also available (note which version you're viewing via the URL bar):</p>
 
         <ul>
             <li><a href="http://jeffpelletier.me">Vanilla HTML/CSS/JS Portfolio</a>.</li>
-            <li><a href="http://stencil.jeffpelletier.me">Stencil Portfolio</a>. More about <a data-hook="link-external" href="https://stenciljs.com/" rel="external">Stencil</a>.</li>
         </ul>
 
         <p>Check out some of my recent <a href="work.html">work</a>, some of the <Link to="/writing">words I've written</Link>, and <a href="mailto:jeffrey.k.pelletier+folio@gmail.com">get in touch</a> if you want to chat.</p>
